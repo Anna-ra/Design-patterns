355a5f0b8995e116577182a6d293cd24d30d690b
Anna Rashchupkina <anna_rashchupkina@epam.com>
2022-06-18 17:12:45 +0300

    4. Singleton pattern public class SingletonDriver implemented

--- DesignPatterns/src/main/java/driver/SingletonDriver.java
+++ DesignPatterns/src/main/java/driver/SingletonDriver.java
@@ -2,7 +2,7 @@ package driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class SingletonDriver {
     private static WebDriver driver;
@@ -12,9 +12,9 @@ public class SingletonDriver {
 
     public static WebDriver getWebDriverInstance() {
         if (driver == null) {
-            ChromeOptions options = new ChromeOptions();
-            options.setCapability("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-            driver = new ChromeDriver(options);
+            System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+            DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+            driver = new ChromeDriver(capabilities);
         }
         return driver;
     }
